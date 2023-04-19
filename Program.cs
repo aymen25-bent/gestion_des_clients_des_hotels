ee264c9a88a36c13c1a30efa9fc17002dcaba28b
aymen25-bent <aymen.bentounsi@hotmail.com>
2023-04-18 22:08:01 -0400

    final version

--- Program.cs
+++ Program.cs
@@ -48,10 +48,13 @@ namespace GestionClient
     {
         static void Main()
         {
+            Console.WriteLine("======================================================================= ");
+            Console.WriteLine("Bienvenue A HOTEL");
+            Console.WriteLine("======================================================================= ");
             // Exemple d'utilisation
-            Hotel hotel = new Hotel("Le Ritz", "1234 Rue de la Paix");
-            Client client1 = new Client("Doe", "John", "johndoe@example.com", "1234-5678-9012-3456", 101);
-            Client client2 = new Client("Smith", "Jane", "janesmith@example.com", "2345-6789-0123-4567", 102);
+            Hotel hotel = new Hotel("Le Parisien", "6261 Rue du bordeaux");
+            Client client1 = new Client("aymen", "bentounsi", "aymen@gmail.com", "438-274-7801", 101);
+            Client client2 = new Client("saber ", "ayoub", "ayoubh@gmail.com", "456-371-8293", 102);
 
             hotel.AjouterClient(client1);
             hotel.AjouterClient(client2);
@@ -60,8 +63,10 @@ namespace GestionClient
             {
                 Console.WriteLine("{0} {1} ({2}), chambre {3}", client.Prenom, client.Nom, client.Courriel, client.NumChambre);
             }
+            onsole.WriteLine("======================================================================= ");
+            Console.WriteLine("Appuyez sur n'importe quelle touche pour quitter.");
+            Console.WriteLine("======================================================================= ");
 
             Console.ReadLine();
-        }
+        } }
     }
-}
